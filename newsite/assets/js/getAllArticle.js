e9f2067c1bbf04161d95e8541bc06f93bd519ec1
Allison An <allison.an@arup.com>
2021-02-05 10:44:20 +0800

    test

--- newsite/assets/js/getAllArticle.js
+++ newsite/assets/js/getAllArticle.js
@@ -2,14 +2,14 @@ function myFunction() {
 	$.get('./assets/article/archive.json', function (res) {
 		res.data.forEach(element => {
 			console.log(element)
-			$("#post").prepend(`<article>
+			$("#post").append(`<article>
 					<header>
 						<span class="date">`+ element.date + `</span>
 						<h2>`+ element.title + `</h2>
 					</header>
-					<p>`+ element.intro + `</p>
+					<p style="text-align:center">`+ element.intro + `</p>
 					<ul class="actions special">
-						<li><a href="`+ element.url + `" class="button">Full Story</a></li>
+						<li><a href="`+ 'assets\\article\\' + element.url + `" class="button">Full Story</a></li>
 					</ul>
 				</article>`)
 		});
